93445a5531e2b7fc3d05087518be859a4bc03113
Tanner McDaniel <tmmcdaniel909@gmail.com>
2023-09-21 17:01:04 +0000

    got logic in javascript to work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+const stateArray = [0,1,2,3,4,5];
+let currentStateIndex = 0;
 
+let array1 = ["I can read your mind", "Pick a number from 01-99", "Add both digits together to get a new number", "Subtract your new number from the original number", "", "&" ];
+let array2 = ["", "when you have your number click next", "Ex: 14 is 1 + 4 = 5 click next to proceed", "Ex: 14 - 5 = 9 click next to proceed", "Find you new number. Note the symbol beside the number", "Your symbol is: &"];
+
+const primaryText = document.getElementById("primaryText");
+const subText = document.getElementById("subText");
+const nextButton = document.getElementById("nextButton");
+
+nextButton.addEventListener('click', changePage);
+
+function changePage() {
+    if (currentStateIndex < stateArray.length) {
+        primaryText.textContent = array1[currentStateIndex];
+        subText.textContent = array2[currentStateIndex];
+        currentStateIndex++;
+    } else {
+        currentStateIndex = 0;
+        primaryText.textContent = array1[currentStateIndex];
+        subText.textContent = array2[currentStateIndex];
+    }
+}
+
+primaryText.textContent = array1[currentStateIndex];
+subText.textContent = array2[currentStateIndex];
