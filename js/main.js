94a9ee27feccb1ccbd7410e11be1cebee9f91946
Tanner <tmmcdaniel909@gmail.com>
2023-11-24 12:46:15 -0500

    final fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const stateArray = [0,1,2,3,4,5];
 let currentStateIndex = 0;
 
-let array1 = ["I can read your mind", "Pick a number from 01-99", "Add both digits together to get a new number", "Subtract your new number from the original number", "", "&" ];
-let array2 = ["", "when you have your number click next", "Ex: 14 is 1 + 4 = 5 click next to proceed", "Ex: 14 - 5 = 9 click next to proceed", "Find you new number. Note the symbol beside the number", "Your symbol is: &"];
+let array1 = ["I can read your mind", "In your head, pick a number from 01-99", "Add both digits together to get a new number", "Subtract your new number from the original number", "", "&" ];
+let array2 = ["", "When you have your number click next", "Ex: 14 is 1 + 4 = 5 click next to proceed", "Ex: 14 - 5 = 9 click next to proceed", "Find you new number. Note the symbol beside the number", "Your symbol is: &"];
 
 const primaryText = document.getElementById("primaryText");
 const subText = document.getElementById("subText");
@@ -11,7 +11,7 @@ const nextButton = document.getElementById("nextButton");
 primaryText.textContent = array1[currentStateIndex];
 subText.textContent = array2[currentStateIndex];
 
-const arrayOfSymbols = [ "@", "#", "$", "%", "^", "*", "+", "!", "&"];
+const arrayOfSymbols = [ " = @", " = #", " = $", " = %", " = ^", " = *", " = +", " = !", " = &"];
 const divisbleBy9Symbol = arrayOfSymbols.splice(Math.floor(Math.random()*9),1);
 
 function updateText(){
@@ -32,8 +32,9 @@ function updateText(){
         subText.textContent = array2[currentStateIndex];
     }
     } else if (currentStateIndex === 5){
-        primaryText.textContent = divisbleBy9Symbol;
-        subText.textContent = "Your symbol is: " + divisbleBy9Symbol;
+        const finalSymbol = divisbleBy9Symbol[0].split(" = ")[1];
+        primaryText.textContent = finalSymbol;
+        subText.textContent = "Your symbol is: " + finalSymbol;
     } else {
         primaryText.textContent = array1[currentStateIndex];
         subText.textContent = array2[currentStateIndex];
